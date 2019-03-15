09d92c021dc1f0a8197f5e899efccbe060173850
Garbin Huang <garbinh@gmail.com>
2019-03-15 22:54:51 +0800

    improve

--- app/servers/scheduler/index.js
+++ app/servers/scheduler/index.js
@@ -3,18 +3,18 @@ import assert from 'assert'
 import getLogger from '../../shared/logger'
 
 const logger = getLogger('scheduler')
-function execute (asyncFunc) {
-  return async function () {
-    try {
-      await asyncFunc()
-    } catch (e) {
-      logger.error(e)
-    }
-  }
-}
 export const jobs = {}
 export async function run (name) {
   const { default: scheduler } = await import(`./jobs/${name}`)
+  function execute (asyncFunc) {
+    return async function () {
+      try {
+        await asyncFunc()
+      } catch (e) {
+        logger.error(e)
+      }
+    }
+  }
   const job = jobs[name] = schedule.scheduleJob(scheduler.name, scheduler.schedule, execute(scheduler.handler))
   job.on('run', execute(scheduler.events.run))
   job.on('scheduled', execute(scheduler.events.scheduled))
